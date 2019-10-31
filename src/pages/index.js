0b567c31c9ef667acf6e65f80824c6fe1ec5cd58
Kalpesh Singh <kalpesin@amazon.com>
2019-10-31 14:49:43 +0530

    feat(Homepage): adds Cards on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,73 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
+import PAGE_TYPE from "./../core/constants/pageType"
+import Header from "../components/Header"
+import Jumbotron from "../components/Jumbotron"
+import HighlightedText from "../components/HighlightedText"
+import Card from "../components/Card"
+
+import "./index.css"
+
+import beforeYouGo from "./../images/japan_airline_ticket.jpg"
+import duringTravel from "./../images/japan_airport_welcome_board.jpg"
+import exitTourism from "./../images/tokyo_station.jpg"
+
 const IndexPage = () => (
-  <Layout>
-    {/*<SEO title="Home" />*/}
+  <Layout type={PAGE_TYPE.HOMEPAGE}>
+    <Header />
+    <Jumbotron
+      title={<div>Read Kalpesh’s Japan <div>experiences.</div></div>}
+      subtitle={
+        <>
+          <HighlightedText>3</HighlightedText> cities,&nbsp;
+          <HighlightedText>11</HighlightedText> days and unlimited memories.
+        </>
+      }
+    />
+    <h2 className="homepage__section-title">Preparation</h2>
+    <div className="cards">
+      <Card
+        title={<HighlightedText>Before you go</HighlightedText>}
+        image={beforeYouGo}
+      />
+      <Card
+        title={<HighlightedText>During travel</HighlightedText>}
+        image={duringTravel}
+      />
+      <Card
+        title={<HighlightedText>Exit tourism</HighlightedText>}
+        image={exitTourism}
+      />
+    </div>
+
+    <h2 className="homepage__section-title">Read experiences</h2>
+    <div className="cards">
+      <Card
+        title={
+          <div>
+            <HighlightedText>Tokyo,</HighlightedText> 5 days
+          </div>
+        }
+        image={beforeYouGo}
+      />
+      <Card title={<div>
+        <HighlightedText>Osaka,</HighlightedText> 3 days
+      </div>} image={duringTravel} />
+      <Card title={<div>
+        <HighlightedText>Kyoto,</HighlightedText> 3 days
+      </div>} image={exitTourism} />
+    </div>
+
+    <SEO title="Home" />
     {/*<h1>Hi people</h1>*/}
     {/*<p>Welcome to your new Gatsby site.</p>*/}
     {/*<p>Now go build something great.</p>*/}
     {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>*/}
-      {/*<Image />*/}
+    {/*<Image />*/}
     {/*</div>*/}
     {/*<Link to="/page-2/">Go to page 2</Link>*/}
   </Layout>
