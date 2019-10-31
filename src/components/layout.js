19362e49e9479ad5b94d8b96664abceefa26b286
Kalpesh Singh <kalpesin@amazon.com>
2019-10-31 14:49:17 +0530

    refactor(layout): adds Grid layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,13 +8,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./Header"
-import Jumbotron from "./Jumbotron"
-import HighlightedText from "./HighlightedText"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+const Layout = ({ children, type }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -26,33 +22,14 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <React.Fragment>
-      <Header />
-      <Jumbotron
-        title={"Read Kalpesh’s Japan experiences."}
-        subtitle={
-          <>
-            <HighlightedText>3</HighlightedText> cities,&nbsp;
-            <HighlightedText>11</HighlightedText> days and unlimited memories.
-          </>
-        }
-      />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        {/*<footer>*/}
-        {/*© {new Date().getFullYear()}, Built with*/}
-        {/*{` `}*/}
-        {/*<a href="https://www.gatsbyjs.org">Gatsby</a>*/}
-        {/*</footer>*/}
-      </div>
-    </React.Fragment>
+    <div className={type}>
+      <main>{children}</main>
+      {/*<footer>*/}
+      {/*© {new Date().getFullYear()}, Built with*/}
+      {/*{` `}*/}
+      {/*<a href="https://www.gatsbyjs.org">Gatsby</a>*/}
+      {/*</footer>*/}
+    </div>
   )
 }
 
