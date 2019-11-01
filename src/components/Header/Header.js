85554a60e88e57af7b30618fdbc4046eda3fe34d
Kalpesh Singh <kalpesin@amazon.com>
2019-11-01 15:06:32 +0530

    feat(Header): highlights header navigation label if on same page and makes it unclickable

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,42 @@ import FilledLink from "./../FilledLink"
 /** styles */
 import "./Header.css"
 
+const HEADER_LINKS = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About Kalpesh",
+    href: "/about-kalpesh",
+  },
+  {
+    label: "Resources",
+    href: "/resources",
+  },
+]
+
+const getLinks = () => {
+  return HEADER_LINKS.map(item => {
+    if (window && window.location.pathname === item.href) {
+      return (
+        <span
+          className="header__navbar__link header__navbar__link--active"
+          key={item.href}
+        >
+          {item.label}
+        </span>
+      )
+    } else {
+      return (
+        <Link className="header__navbar__link" key={item.href} to={item.href}>
+          {item.label}
+        </Link>
+      )
+    }
+  })
+}
+
 const Header = () => (
   <header className="header">
     <h1 className="header__title">Lost In Japan</h1>
@@ -15,11 +51,7 @@ const Header = () => (
       ロストインジャパン
     </h1>
     <nav className="header__navbar">
-      <div className="header__navbar__left">
-        <Link to="/">Home</Link>
-        <Link to="/about-kalpesh">About Kalpesh</Link>
-        <Link to="/Resources">Resources</Link>
-      </div>
+      <div className="header__navbar__left">{getLinks()}</div>
       <div className="header__navbar__right">
         <FilledLink>Buy me a coffee ☕</FilledLink>
       </div>
