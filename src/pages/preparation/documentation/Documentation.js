a3d28f4a6ab5b1b02d0a90a315ebdf811df1b752
Kalpesh Singh <kalpesin@amazon.com>
2019-12-07 07:35:34 +0530

    chores(release): polishes documentation page content

--- src/pages/preparation/documentation/Documentation.js
+++ src/pages/preparation/documentation/Documentation.js
@@ -22,7 +22,7 @@ const Documentation = () => (
     <Header />
     <div className="page-container">
       <aside className="page-container__left">
-        <div className="page-container__left-list-title">Documentation</div>
+        <div className="page-container__left-list-title">Documentations</div>
         <ul className="page-container__left-list">
           <li>
             <a href="#important-documents">Important documents</a>
@@ -35,9 +35,9 @@ const Documentation = () => (
       </aside>
       <div className="page-container__right">
         <p>
-          Documentation is a vital part of your trip. Based on the information
-          you record in these documents can help you to assess whether you will
-          enjoy your trip safely or you might need to call off.
+          Documentations are the vital part of your trip. Based on the
+          information you record in these documents will help you to assess
+          whether you will enjoy your trip safely or you might need to call off.
         </p>
 
         <h2 id="important-docs" className="page-container__heading">
@@ -46,27 +46,29 @@ const Documentation = () => (
 
         <ul>
           <li>
-            <Heading level={4}>Trip doc</Heading>
-            Trip doc has your basic details such as your name, address,
+            <Heading level={4}>Trip document</Heading>
+            Trip document has your basic details such as your name, address,
             emergency contacts, blood group, etc. It is vital as I’m ‘O’
             negative blood human and in case of any emergency they can arrange
             for blood. The same goes for any other medical emergency or
-            situation where your basic information is required.{" "}
+            situation where your basic information is required. Please see{" "}
+            <Link to={"/resources/"}>Resources</Link> section to download the
+            sample.
           </li>
 
           <li>
             <Heading level={4}>Weather</Heading>
-            It is always good to prepare a document or remember weather
-            conditions a few days prior and your journey dates. I made a mistake
-            and went to summer in peak Typhoon season. Lesson learned here.
+            It is always good to prepare a document or know weather conditions a
+            few days prior and on your journey dates. I made a mistake and went
+            to summer in peak Typhoon season. Lesson learned here.
             <br />
-            Avoid going to Japan in August and September you will face 36
+            Avoid going to Japan in August and September or you will face 36
             degrees and there is always the danger of typhoons.
           </li>
           <li>
             <Heading level={4}>Day by day itinerary</Heading>
-            Prepare a detailed itinerary and help yourself with last moment
-            confusion and decision making. I went to Japan without any
+            Prepare a detailed itinerary and help yourself by avoiding last
+            moment confusion and decision making. I visited Japan without any
             accommodation and trip itinerary. It was always difficult to decide
             and go to places without this. It’s not bravery, it’s stupidity.
           </li>
@@ -92,13 +94,13 @@ const Documentation = () => (
         </h2>
 
         <p>
-          Following are the visa document you will need to submit to the
+          Following are the visa documents you will need to submit to the
           Consulate General of Japan or Embassy of Japan. I applied in Bengaluru
-          and the process is smooth and you will find helpful Indian staff.
+          and the process was smooth and you will find helpful Indian staff.
         </p>
 
         <p>
-          You don’t need to make an appointment you can directly walk-in and
+          You don’t need to make an appointment, you can directly walk-in and
           submit your documents.{" "}
         </p>
 
@@ -119,11 +121,11 @@ const Documentation = () => (
             Visa{" "}
             <a href="https://www.vfsglobal.com/japan/india/delhi/pdf/visa-application.pdf">
               application form
-            </a>
+            </a>{" "}
             to be filled and signed by the applicant
           </li>
           <li>
-            Two recent color photograph with following specification -
+            Two recent color photographs with following specification -
             <ul>
               <li>Matt finish</li>
               <li>45x45 mm</li>
@@ -144,14 +146,14 @@ const Documentation = () => (
           <li>Address proof (I brought my Driving License)</li>
         </ol>
         <p>
-          All the sample documents are attached in{" "}
+          All the sample documents are downloadble from{" "}
           <Link to={"/resources/"}>Resources</Link> section.
         </p>
 
         <List
           list={[
             "Always check if your prints are fitted on paper and not cut. My 30 pages bank statement was cut by one column and I re-printed it again.",
-            "Go to the consulate/embassy early in the morning, I went at 9:00 AM. They open the business hours at 08:30 AM.",
+            "Go to the consulate/embassy early in the morning, I went at 9:00 AM. The business begins at 08:30 AM.",
             "The rental agreement is not considered as your proof. Please take other documents.",
           ]}
         />
