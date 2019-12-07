aaf54e2675d9e49717766fef7e1f66e92ce56c34
Kalpesh Singh <kalpesin@amazon.com>
2019-12-07 13:37:33 +0530

    chores(last-days): polishes last days experience page

--- src/pages/stories/tokyo-last-days/LastDays.js
+++ src/pages/stories/tokyo-last-days/LastDays.js
@@ -13,6 +13,7 @@ import PAGE_TYPE from "./../../../core/constants/pageType"
 /** styles **/
 import "../../stories.css"
 import somewhereInTokyo from "../../../images/somewhere_in_tokyo.jpg"
+import Heading from "../../../components/Heading/Heading"
 
 const LastDays = () => (
   <Layout type={PAGE_TYPE.STORY}>
@@ -32,6 +33,29 @@ const LastDays = () => (
         <div className="page-container__ts">44 sec read time </div>
       </aside>
       <div className="page-container__right">
+        <Heading level={2}>Last words from Kyoto...</Heading>
+
+        <p>
+          I learned new things in Japan and believe me I'm a better person now.
+          <br />
+          On this trip, I came to know that I have to work on a few more points
+          and I believe I will cover them in my next travel.
+        </p>
+        <p>
+          I will soon board an overnight bus to Tokyo and spend 2 nights there.
+          I will go back to India and will again experiment with my schedule. At
+          least I will wake up at 06:30 AM (I do wake now at 06:30 AM daily
+          including weekends).
+        </p>
+        <p>
+          It was a really good time and learning experience in Japan. 🇯🇵 This
+          trip was a complete success for me.
+        </p>
+        <hr />
+        <figure className="page-container__photo">
+          <img src={somewhereInTokyo} />
+          <figcaption>Somewhere in Tokyo</figcaption>
+        </figure>
         <h2 id="day10" className="page-container__heading">
           Day 10{" "}
         </h2>
@@ -42,29 +66,12 @@ const LastDays = () => (
         </h2>
 
         <p>No entry</p>
-        <hr />
 
-        <figure className="page-container__photo">
-          <img src={somewhereInTokyo} />
-          <figcaption>Somewhere in Tokyo</figcaption>
-        </figure>
+        <small>
+          Note: I lost my enthusiasm to write which resulted in no entry on day
+          10 and day 11.
+        </small>
 
-        <p>
-          I’m bored in Japan or I'm bored with traveling. I learned new things
-          and believe me I'm a better person now.
-          <br />
-          On this trip, I came to know that I have to work on a few more points
-          and I believe I will cover them on my next travel.
-        </p>
-        <p>
-          I will soon board an overnight bus to Tokyo and spend 2 nights there.
-          I will go back to India and will again experiment with my schedule. At
-          least I will wake up at 06:30 AM.{" "}
-        </p>
-        <p>
-          It was a really good time and learning experience in Japan. 🇯🇵 This
-          trip was a complete success for me.{" "}
-        </p>
         <FooterBreadcrumb
           left={{
             link: "/stories/kyoto-3-days-stories-itinerary",
