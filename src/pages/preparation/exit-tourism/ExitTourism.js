5f06f540d3df3670e77bcd48a007080b71aaeae6
Kalpesh Singh <kalpesin@amazon.com>
2019-12-07 07:01:01 +0530

    chores(release): polishes exit tourism content

--- src/pages/preparation/exit-tourism/ExitTourism.js
+++ src/pages/preparation/exit-tourism/ExitTourism.js
@@ -22,23 +22,35 @@ const ExitTourism = () => (
       <aside className="page-container__left">
         <div className="page-container__left-list-title">Exit Tourism</div>
         <ul className="page-container__left-list"></ul>
-        <div className="page-container__ts">54 sec read time </div>
+        <div className="page-container__ts">1 minute, 6 seconds</div>
       </aside>
       <div className="page-container__right">
         <p>
-          I’m trying to live a minimalist life and when it comes to shopping, I
-          have to choose stuff very carefully as it will pile and will break my
-          vision of being minimalist. I wanted to buy a trouser and I found that
-          I can buy in Japan which will be useful in daily life as savourer. I
-          didn't buy in Japan for others. Though I bought one big and one small
-          bottle of sake. Keep your shopping and purchase at the end of two
-          days. As it was an 11 days trip and I thought I will spend two days at
-          my place to do the shopping and also will visit a few places. In my
-          plan, I already came to know about 8% of consumer tax benefits to
-          tourism in Japan. It was pretty straightforward and I took the
-          benefit. (Though added a fitness band to save 250 INR. Agh! Minimalist
-          life.)
+          I’m trying to live a minimalist life. When it comes to shopping I have
+          to choose stuff very carefully as it will pile and break my vision of
+          being minimalist.
         </p>
+        <p>
+          I needed a trouser and I realized that I can align that with shopping
+          in Japan which will be useful in daily life. A souvenir I love to
+          wear.
+        </p>
+
+        <p>
+          I didn't buy for others (except for siblings). Though I bought one
+          large and one small bottle of sake. I kept my shopping and purchases
+          at the end of two days. As it was an 11 days trip and I thought I will
+          spend two days in Tokyo to do the shopping and also will visit a few
+          places.
+        </p>
+
+        <p>
+          In my research, I already came to know about 8% of consumer tax
+          benefits to tourism in Japan. It was pretty straightforward and I took
+          the benefit. (Though added a fitness band to save 250 INR. Agh!
+          Minimalist life.)
+        </p>
+
         <p>
           Derma recommended me Uniqlo (Japanese clothing brand) which sells
           reasonably priced clothes. I bought a fantastic trouser and I’m
@@ -51,7 +63,7 @@ const ExitTourism = () => (
             "See if you can match the shopping with your current needs",
             "Research on stuff if they are allowed on the airplane",
             "Always tell shoppers that you will take it back to the home\n" +
-              "              country so that they can do extra packing",
+              "              country to get extra packing",
           ]}
         />
         <FooterBreadcrumb
